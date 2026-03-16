c397036620367b84e673762e8d53f152aaafe27b
Ing. molina <ivanmolinrev@gmail.com>
2026-03-16 10:34:51 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from crewai import Agent
+
+app = Flask(__name__)
+
+@app.route(, methods=[GET,POST])
+def home()
+    respuesta = 
+    if request.method == POST
+        idea = request.form[idea]
+        respuesta = fProyecto generado sobre {idea}
+    return f
+    h1Agente IA Proyectosh1
+    form method=post
+    Idea del proyectobr
+    input name=idea
+    brbr
+    buttonGenerarbutton
+    form
+    h3{respuesta}h3
+    
+
+app.run()
